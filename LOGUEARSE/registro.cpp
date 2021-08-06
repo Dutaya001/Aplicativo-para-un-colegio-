b38904571234861e3d3090fae5c4c316ed7a60bb
luissfernando <82791279+luissfernando@users.noreply.github.com>
2021-08-05 19:56:30 -0500

    uniendo_mis_archivos_de_la_BasedeDatos

--- LOGUEARSE/registro.cpp
+++ LOGUEARSE/registro.cpp
@@ -4,6 +4,9 @@
 #include "QString"
 #include "QSqlQuery"
 #include "QtSql/QSqlError"
+#include <iostream>
+#include <fstream>
+using namespace std;
 Registro::Registro(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Registro)
@@ -15,6 +18,101 @@ Registro::Registro(QWidget *parent) :
 Registro::~Registro()
 {
     delete ui;
+}
+void Registro::agregar(){
+    ofstream escritura; //escribir  fichero(entrada)
+    escritura.open("final.csv",ios::app);
+    tam++;
+    cout<<"elija contraseña password ";
+    getline(cin,password);
+    cout<<"ingrese codigo :";
+    getline(cin,codigo);
+    cout<<"ingrese nombre :";
+    getline(cin,nombre);
+    cout<<"ingrese telefono : ";
+    cin>>fono;
+    cin.ignore();
+    cout<<"ingrese direccion : ";
+    getline(cin,direccion);
+    cout<<"ingrese sexo :";
+    cin>>sexo;
+    cin.ignore();
+    cout<<"tiene conexion a internet? :";
+    getline(cin,internet);
+    cout<<"presenta covid? :";
+    getline(cin,covid);
+
+    escritura<<password<<","<<codigo<<","<<nombre<<","<<fono<<","<<direccion<<","<<sexo<<","<<internet<<","<<covid<<"\n";
+    escritura.close();
+        //mostrar datos agregados
+    cout<<"\n\t\tDatos agregados :\n";
+    cout<<"contraseña : "<<password<<"codigo : "<<codigo<<"\nnombre : "<<nombre<<"\ntelefono : "<<fono<<"\ndireccion : "<<direccion<<"\nsexo : "<<sexo<<"\nPresenta internet : "<<internet<<"\nPresenta covid : "<<covid<<"\nContraseña : "<<password<<"\n";
+}
+int Registro::buscar(string _password, string _codigo){
+    ifstream leer;
+    string linea;
+    int busq_linea=1;
+    char coma = ',';
+    leer.open("final.csv",ios::in);
+    getline(leer,linea); // paso de linea
+
+    while (getline(leer,linea)){
+        int i=0;
+        int j=0;
+        while(linea[i] != coma){
+            if(linea[i] != _password[i]){
+                break;
+            }
+            i++;
+            }
+        i++;
+        if(i == _password.size()+1){
+            while (linea[i] != coma){
+                if(linea[i] != _codigo[j]){
+                    break;
+                }
+                i++;
+                j++;
+            }
+            if(j==_codigo.size()){
+                return busq_linea;
+            }
+        }
+        busq_linea++;
+    }
+    leer.close();
+    return 0;
+}
+void Registro::eliminar(int line){
+    ifstream leer;  //leer
+    ofstream temp;    //modificar
+    string linea;
+    leer.open("final.csv",ios::in);
+    temp.open("temp.csv",ios::out);
+    for(int i=0;i<tam;i++){
+        if(i==line){
+            cout<<"eliminado = ";
+            getline(leer,linea);
+            cout<<linea<<endl;
+        }
+        getline(leer,linea);
+        temp<<linea;
+
+    }
+    temp.close();
+    leer.close();
+    remove("final.csv");
+    rename("temp.csv","final.csv");
+}
+void Registro::listarTodos(){
+    ifstream leer;
+    string linea;
+    leer.open("listado.csv",ios::in);
+    getline(leer,linea);
+    while(getline(leer,linea)){
+        cout<<linea<<"\n";
+    }
+    leer.close();
 }
 void Registro::CrearTabla(){
     QString consulta;
--- LOGUEARSE/registro.h
+++ LOGUEARSE/registro.h
@@ -15,11 +15,24 @@ public:
     explicit Registro(QWidget *parent = nullptr);
     ~Registro();
     void CrearTabla();
+    void agregar();
+    int buscar(std::string _password,std::string _codigo);
+    void eliminar(int);
+    void listarTodos();
 private slots:
     void on_pushButton_clicked();
 
 private:
     Ui::Registro *ui;
+    int tam=5;
+    std::string password;
+    std::string codigo;
+    std::string  nombre;
+    int fono;
+    std::string direccion;
+    char sexo;
+    std::string internet;
+    std::string covid;
 };
 
 #endif // REGISTRO_H
